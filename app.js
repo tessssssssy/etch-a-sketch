2c8e7deaebfc3b98aa1bf8cf6b72f289d3747851
tessssssssy <tessfabry@gmail.com>
2019-11-12 21:48:27 +1100

    new canvas css variables

--- app.js
+++ app.js
@@ -1,21 +1,55 @@
 //select container
 var container = document.querySelector("#container");
 var clearBtn = document.querySelector("#clear");
+var newBtn = document.querySelector("#new");
 //create child div
 //var square = document.createElement("div");
 
-//initiate grid
-createGrid();
+//get css variables
+let htmlStyles = window.getComputedStyle(document.querySelector("html"));
+let rowNum = parseInt(htmlStyles.getPropertyValue("--rowNum"));
+let colNum = parseInt(htmlStyles.getPropertyValue("--colNum"));
+let squareH = parseInt(htmlStyles.getPropertyValue("--squareH"));
+let squareW = parseInt(htmlStyles.getPropertyValue("--squareW"));
+//set style variables
+//document.documentElement.style.setProperty("--rowNum", 6);
 
-function createGrid() {
-  for (var i = 0; i < 256; i++) {
+//initiate grid
+createGrid(16);
+
+function createGrid(width) {
+  //var width = Number(prompt("Canvas Width?"));
+  document.documentElement.style.setProperty("--rowNum", width);
+  document.documentElement.style.setProperty("--colNum", width);
+  document.documentElement.style.setProperty("--squareH", 900 / width);
+  document.documentElement.style.setProperty("--squareW", 900 / width);
+  for (var i = 0; i < width * width; i++) {
     var square = document.createElement("div");
     container.appendChild(square);
     square.classList.add("square");
   }
 }
-
+//mouseover effect
 var squares = document.querySelectorAll(".square");
+
+//new canvas
+newBtn.addEventListener("click", function() {
+  clearCanvas();
+  var width = parseInt(htmlStyles.getPropertyValue("--rowNum"));
+  for (var i = 0; i < width * width; i++) {
+    container.removeChild(squares[i]);
+  }
+  createGrid(Number(prompt("Canvas Width?")));
+
+  squares = document.querySelectorAll(".square");
+
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].addEventListener("mouseover", function() {
+      this.style.background = "black";
+    });
+  }
+});
+
 //add event listener to squares
 for (var i = 0; i < squares.length; i++) {
   squares[i].addEventListener("mouseover", function() {
@@ -24,7 +58,6 @@ for (var i = 0; i < squares.length; i++) {
 }
 
 //clear canvas
-
 function clearCanvas() {
   for (var i = 0; i < squares.length; i++) {
     squares[i].style.background = "grey";
